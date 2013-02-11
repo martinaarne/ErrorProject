c53ee505b76e1c290fa1ff4035aa26a953a6c09b
Martin Aarne <maarne@itcollege.ee>
2013-02-11 23:58:11 +0200

    Added project to GitHub

---
+++ ErrorProject/Controllers/ErrorController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ErrorProject.Models;
+
+namespace ErrorProject.Controllers
+{
+    public class ErrorController : Controller
+    {
+        private ErrorDBEntities db = new ErrorDBEntities();
+
+        //
+        // GET: /Error/
+
+        public ActionResult Index()
+        {
+            return View(db.Errors.ToList());
+        }
+
+        //
+        // GET: /Error/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Error/Create
+
+        [HttpPost]
+        public ActionResult Create(Error error)
+        {
+            if (ModelState.IsValid && error.Name != null)
+            {
+                db.Errors.Add(error);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(error);
+        }
+
+        //
+        // GET: /Error/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Error error = db.Errors.Find(id);
+            if (error == null)
+            {
+                return HttpNotFound();
+            }
+
+            //Otsin LINQ-ga kõik kommentaarid, mis antud Erroril on
+            var query = from x in db.Comments
+                        where x.ErrorID == id
+                        select x;
+
+            ViewBag.Comments = query.ToList();
+
+            return View(error);
+        }
+
+        //
+        // POST: /Error/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Error error)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(error).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(error);
+        }
+
+        //
+        //GET: /Error/AddComment/5
+
+        public ActionResult AddComment(int id = 0)
+        {
+            Comment comment = new Comment();
+            comment.ErrorID = id;
+            return View(comment);
+        }
+
+        //
+        //POST: /Error/AddComment/5
+
+        [HttpPost]
+        public ActionResult AddComment(Comment comment)
+        {
+            if (ModelState.IsValid && comment.Text != null)
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+
+                //Redirectin tagasi errori editimise lehele
+                return RedirectToAction("Edit/" + comment.ErrorID);
+            }
+
+            return View(comment);
+        }
+
+        //
+        // GET: /Error/Search
+
+        public ActionResult Search()
+        {
+                return View();
+        }
+
+        //
+        //POST: /Error/Search/5
+
+        public ActionResult Search(int id = 0)
+        {
+            Error error = db.Errors.Find(id);
+
+            return View(error);
+        }
+    }
+}
---
+++ ErrorProject/Models/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ErrorProject.Models
+{
+    public class ViewModel
+    {
+        private Error _error;
+        private List<Comment> _comments;
+
+        public Error Error
+        {
+            get { return _error; }
+            set { _error = value; }
+        }
+
+        public List<Comment> Comments
+        {
+            get { return _comments; }
+            set { _comments = value; }
+        }
+    }
+}
